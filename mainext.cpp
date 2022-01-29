3adf5b3f629c99dbf08844e6fbbe753dde06a674
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-01-29 16:51:29 +0100

    Update mainext.cpp

--- mainext.cpp
+++ mainext.cpp
@@ -5,25 +5,25 @@
 
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-void showHeader(std::string ExtensionName, std::string Path, bool isUser) {
+void showHeader(std::string ExtensionName, std::string Path, std::string Name) {
     SetConsoleTextAttribute(hConsole, 2);
     std::cout << ExtensionName;
     SetConsoleTextAttribute(hConsole, 10);
     std::cout << " " << Path << "\n";
     SetConsoleTextAttribute(hConsole, 15);
     
-    if (isUser) {
-        SetConsoleTextAttribute(hConsole, 9);
-        std::cout << "User";
+
+    if (Name == "Root") {
+        SetConsoleTextAttribute(hConsole, 12);
+        std::cout << Name;
         SetConsoleTextAttribute(hConsole, 15);
-        std::cout << " > ";
     }
     else {
-        SetConsoleTextAttribute(hConsole, 12);
-        std::cout << "Root";
+        SetConsoleTextAttribute(hConsole, 9);
+        std::cout << Name;
         SetConsoleTextAttribute(hConsole, 15);
-        std::cout << " > ";
     }
+    std::cout << " > ";
 }
 
 bool isPath(std::string &path, std::string command) {
@@ -41,4 +41,21 @@ bool isPath(std::string &path, std::string command) {
         SetConsoleTextAttribute(hConsole, 15);
         return false;
     }
+}
+
+void errorText(std::string text) {
+    SetConsoleTextAttribute(hConsole, 12);
+    std::cout << text << "\n";
+    SetConsoleTextAttribute(hConsole, 15);
+}
+
+void showHelp() {
+    std::cout << "\tEXTENSIONS\n\n" 
+        << "\text set [name] [path]   - copy the file at path and save it as name in the extension file\n"
+        << "\text del [name]          - delete the extension\n"
+        << "\text run [name]          - run the extension\n"
+        << "\text list                - list all extention name and file linked with them\n\n"
+        << "\tOTHER\n\n"
+        << "\tcat [file]              - view the file content\n"
+        << "\tcompare [file1] [file2] - comapre two files and show the differences\n";
 }
