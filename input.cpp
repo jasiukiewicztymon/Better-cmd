6ba0063e4ef0a2f8dc99a5f2e574c5384e737210
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-02-04 23:45:35 +0100

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -3,8 +3,37 @@
 #include <iostream>
 #include <conio.h>
 #include <stdio.h>
+#include <vector>
+#include <map>
 
-void CheckKey(void) {
+/*
+	Arguments: To use all the possibilities of the bcin
+	function and get correction and syntax help pass a vector
+	of command with the map key as the command and the vector
+	of string as parameter of possibilities, next pass the
+	variable vector, list vector, function vector (having a
+	pointer to the function from a class)
+*/
+
+std::string output = "";
+char move;
+int cox = 0, coy = 0;
+
+void CheckKey();
+
+std::string bcin(std::map<std::string, std::vector<std::string>> commandSyntax, std::vector<std::string> variable, std::vector<std::vector<std::string>> list, std::vector<std::vector<std::string>> function) {
+	std::vector<std::string> keywords;
+
+	for (std::map<std::string, std::vector<std::string>>::iterator it = commandSyntax.begin(); it != commandSyntax.end(); ++it) {
+		keywords.emplace_back(it->first);
+	}
+
+    while (1) {
+
+    }
+}
+
+void CheckKey() {
     int key;
     if (_kbhit()) {
         key = _getch();
@@ -13,31 +42,25 @@ void CheckKey(void) {
                 key = _getch();
             } while (key == 224);
             switch (key) {
-            case 72:
-                printf("up");
-                break;
             case 75:
-                printf("left");
+                move = 'l';
                 break;
             case 77:
-                printf("right");
-                break;
-            case 80:
-                printf("down");
+                move = 'r';
                 break;
             }
         }
         else {
             char ch = key;
-            std::cout << ch << "\n";
-        }
-    }
-}
-
-int main() {
-    while (1) {
-        if (_kbhit()) {
-            CheckKey();
+            if (ch == 8) {
+                // backspace
+            }
+            if (ch == 127) {
+                // delete
+            }
+            else {
+                output += ch;
+            }
         }
     }
 }
