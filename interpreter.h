e06b198a07a75b05b01a997d7dc25572b64c046c
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-03-21 19:51:54 +0100

    Create interpreter.h

---
+++ interpreter.h
@@ -0,0 +1,12 @@
+#include <map>
+#include <vector>
+#include <string>
+
+void interpret(
+	std::string&,
+	std::map<std::string, std::string>&,
+	std::map<std::string, std::vector<std::string>>&,
+	std::string&,
+	int&,
+	int&
+);
